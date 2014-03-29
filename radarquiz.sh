ad5d2d2a826032027594c4b46e1365667f38ab3e
PetrZapletal <zapletal.petr1@gmail.com>
2014-03-29 20:09:27 +0100

    curl fixed

--- radarquiz.sh
+++ radarquiz.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
-TARGET=`curl -s http://www.footballradar.com/quiz/ | grep -E -o [\-]?[0-9][0-9].[\*\+\-].[0-9][0-9].[\*\+\-].[0-9][0-9] | bc`
+TARGET=`curl -c radarcookie.txt -s http://www.footballradar.com/quiz/ | grep -E -o [\-]?[0-9][0-9].[\*\+\-].[0-9][0-9].[\*\+\-].[0-9][0-9] | bc`
 
 wget -x --load-cookies radarcookie.txt http://www.footballradar.com/quiz/answer/${TARGET}
 
